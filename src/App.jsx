80b448b58fbd9bb9ea7c9d4531abf44be4b77385
imanee11 <imaneelouannane@gmail.com>
2025-08-30 14:58:53 +0100

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Tailwind is working!
+      </h1>
     </>
   );
 };
