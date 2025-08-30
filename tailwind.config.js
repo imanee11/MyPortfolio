42183be454e5ccfe8e4519790e57223d43718ea0
imanee11 <imaneelouannane@gmail.com>
2025-08-30 15:06:28 +0100

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        syne: ['"Syne"', 'sans-serif'],
+      },
+      backgroundImage: {
+        'gradient-light': 'linear-gradient(15deg, #8f56cc 0%, #5d4ec4 80%)',
+        'gradient-dark': 'linear-gradient(15deg, #E1BAC5, #CEC4EF)',
+      }
+    },
   },
   plugins: [],
 }
