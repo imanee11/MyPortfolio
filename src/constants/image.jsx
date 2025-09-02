2091aa60d4cf51feb5fbb97e98509e69585165c3
imanee11 <imaneelouannane@gmail.com>
2025-09-02 13:07:37 +0100

    add constants

---
+++ src/constants/image.jsx
@@ -0,0 +1,60 @@
+import fashe from "../assets/images/fashe/fashe.jpeg";
+import fashe2 from "../assets/images/fashe/fashe2.jpeg";
+import fashe3 from "../assets/images/fashe/fashe3.jpeg";
+import fashe4 from "../assets/images/fashe/fashe4.jpeg";
+import fashe5 from "../assets/images/fashe/fashe5.jpeg";
+import fashe6 from "../assets/images/fashe/fashe6.jpeg";
+import fashe7 from "../assets/images/fashe/fashe7.jpeg";
+
+import imane from "../assets/images/imanePortfolio/imanePortfolio.jpeg";
+import imane2 from "../assets/images/imanePortfolio/imanePortfolio2.jpeg";
+import imane3 from "../assets/images/imanePortfolio/imanePortfolio3.jpeg";
+
+import restaurant from "../assets/images/restaurant/Restaurant.jpeg";
+import restaurant2 from "../assets/images/restaurant/Restauran2.jpeg";
+import restaurant3 from "../assets/images/restaurant/Restaurant3.jpeg";
+import restaurant4 from "../assets/images/restaurant/Restaurant4.jpeg";
+import restaurant5 from "../assets/images/restaurant/Restaurant5.jpeg";
+
+import task from "../assets/images/task/landingPage.jpeg";
+import task2 from "../assets/images/task/task2.jpeg";
+import task3 from "../assets/images/task/task3.jpeg";
+import task4 from "../assets/images/task/task4.jpeg";
+
+import yummy from "../assets/images/yummy/yummy.jpeg";
+import yummy2 from "../assets/images/yummy/yummy2.jpeg";
+import yummy3 from "../assets/images/yummy/yummy3.jpeg";
+import yummy4 from "../assets/images/yummy/yummy4.jpeg";
+import yummy5 from "../assets/images/yummy/yummy5.jpeg";
+
+import mor from "../assets/images/morocco.jpeg";
+import mor2 from "../assets/images/morocco2.jpeg";
+
+export default {
+    fashe,
+    fashe2,
+    fashe3,
+    fashe4,
+    fashe5,
+    fashe6,
+    fashe7,
+    imane,
+    imane2,
+    imane3,
+    restaurant,
+    restaurant2,
+    restaurant3,
+    restaurant4,
+    restaurant5,
+    task,
+    task2,
+    task3,
+    task4,
+    yummy,
+    yummy2,
+    yummy3,
+    yummy4,
+    yummy5,
+    mor,
+    mor2,
+};
