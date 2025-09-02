4085f1bea55157192494804f3a79444cb26b33d5
imanee11 <imaneelouannane@gmail.com>
2025-09-02 13:11:01 +0100

    add portfolio section

--- src/components/home/Portfolio.tsx
+++ src/components/home/Portfolio.tsx
@@ -1,9 +1,9 @@
 import React, { useContext, useState, useCallback } from "react";
 import { BsStars } from "react-icons/bs";
 import { DarkModeContext } from "../../context/DarkModeContext";
-// import image from "../../constants/image"; // imported images
-// import { projects } from "../../constants";
-// import SkeletonLoader from "./animation/SkeletonLoader";
+import image from "../../constants/image"; // imported images
+import { projects } from "../../constants";
+import SkeletonLoader from "./animation/SkeletonLoader";
 import { useNavigate } from "react-router-dom";
 
 const Portfolio = () => {
@@ -36,7 +36,46 @@ const Portfolio = () => {
       </div>
 
       {/* Projects Grid */}
+      <div className="pt-6 flex flex-wrap gap-4 justify-between">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="relative rounded-3xl overflow-hidden w-full md:w-[48%] mb-6 transform-gpu"
+            onClick={() => navigate(`/details/${project.id}`)}
+          >
+            <div className="relative w-full h-[370px]">
+              {!isImageLoaded(project.id) && (
+                <div className="absolute inset-0 z-10">
+                  <SkeletonLoader />
+                </div>
+              )}
 
+              <img
+                src={image[project.images[0]]}
+                alt={project.title}
+                className={`w-full h-full object-cover object-top transition-opacity duration-500 ${!isImageLoaded(project.id) ? 'opacity-0' : 'opacity-100'}`}
+                loading="lazy"
+                onLoad={() => handleImageLoad(project.id)}
+                onError={() => handleImageLoad(project.id)}
+              />
+
+              {/* Tools */}
+              <div className="absolute bottom-3 w-full px-4">
+                <div className="flex flex-wrap gap-2">
+                  {project.tools.map((tool, idx) => (
+                    <span
+                      key={idx}
+                      className="text-[10px] font-semibold px-3 py-2 rounded-full text-[#22232c] bg-[#e6ebf4]"
+                    >
+                      {tool}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
 import images from "./image"
 export {images}
+
+import projects from '../json/data.json'
+export {projects}
